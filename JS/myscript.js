98ff6917e2b517d269e21869d181de270d705781
kokanevina <kokane.vina@gmail.com>
2023-12-11 15:49:38 +0530

    files coding changed

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,12 +1,15 @@
-let years=[2022,2023,2024,2025];
+let years=[2022,2023];
 const months=["January", "February", "March", "April", "May",
 "June", "July", "August", "September", "October", "November", "December"];
 let calenderMap=new Map();
-calenderMap.set('Training Calendar - November 2023', 'https://drive.google.com/file/d/1jp9gAOm-Xv7idRAEpUZqN3xRdN7JsGFS/preview');
-calenderMap.set('Employee Connect Calendar - November 2023', 'https://drive.google.com/file/d/1ey9fHhNDQM_a0WB0vp9tUKpx04AmJu5L/preview');
-calenderMap.set('Engagement Calendar - November 2023','https://drive.google.com/file/d/1I0_pqOgpV_7ra1JLEYSzOv3ATHTe8k_W/preview')
+calenderMap.set('Training Calendar November 2023', 'https://drive.google.com/file/d/1jp9gAOm-Xv7idRAEpUZqN3xRdN7JsGFS/preview');
+calenderMap.set('HRBP Connect Calendar November 2023', 'https://drive.google.com/file/d/1ey9fHhNDQM_a0WB0vp9tUKpx04AmJu5L/preview');
+calenderMap.set('Engagement Calendar November 2023','https://drive.google.com/file/d/1I0_pqOgpV_7ra1JLEYSzOv3ATHTe8k_W/preview')
+calenderMap.set('Training Calendar December 2023','https://drive.google.com/file/d/1vjUNhCBGsNpYVidhoBBwLzr6KVbEdT4k/preview');
 let currentYear=new Date().getFullYear();
-//document.getElementById("year").textContent=currentYear;
+let foundYear=years.find(year=>year==currentYear);
+if(foundYear===undefined)
+    years.push(currentYear);
 let counter=0;
 const currentMonth=new Date().getMonth();
 months.forEach(month=>{
@@ -22,17 +25,25 @@ years.forEach(year=>{
     const optionEle=document.createElement("option");
     optionEle.textContent=year;
     optionEle.value=year;
-    if(currentYear===parseInt(optionEle.value)){
-        optionEle.selected=true;}
+    if(currentYear===parseInt(optionEle.value))
+        optionEle.selected=true;
     document.getElementById("yearselection").append(optionEle);
 })
 const loadCalender=(event)=>{
     const month=months[document.getElementById("monthselection").value];
     const year=document.getElementById("yearselection").value;
     const department=event.target.innerHTML;
-    const mapKey=department.trim()+" Calendar - "+month+" "+year;
+    const mapKey=department.trim()+" Calendar "+month+" "+year;
     console.log(mapKey);
     const calenderLink=calenderMap.get(mapKey);
-    console.log(calenderLink);
-    event.target.href=calenderLink;
+    if(calenderLink!=undefined){
+        event.target.href=calenderLink.trim();
+        document.getElementById("error").textContent="";
+        document.getElementById("calendar").style.display="block";
+    }
+    else  {
+        const errorMessage="No Calendar found for selected Month and Year";
+        document.getElementById("error").textContent=errorMessage;
+        document.getElementById("calendar").style.display="none";
+    }
 }
